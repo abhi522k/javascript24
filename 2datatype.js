701eb8c308aa0c65c5e21f6dbc8d7d8deb8ed329
Abhishek Surse <abhisheksurse5121999.com>
2024-02-17 10:28:17 +0530

    Added more in data type

--- 2datatype.js
+++ 2datatype.js
@@ -17,3 +17,25 @@ console.log(typeof null); // object
 
 // New starting 17/02/2024 for javascript
 
+let value1 = Symbol('123');
+let value2 = Symbol('123');
+console.log(value1)
+console.log(value1 === value2);
+
+//Non preitive datatype
+// Array, Object, functions(object function)
+
+const array_value = ['ironman', 'captamerica', 'hulk', 'thor'];
+
+let obj_value = {
+    name: 'abhi',
+    age: 24,
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof array_value)
+console.log(typeof myfunction)
+console.log(typeof obj_value)
