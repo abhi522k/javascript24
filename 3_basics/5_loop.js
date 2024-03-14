e03d4979ab14290458483f4275a184dbcd7b1830
Abhishek Surse <abhisheksurse5121999.com>
2024-03-14 20:36:51 +0530

    new change

--- 3_basics/5_loop.js
+++ 3_basics/5_loop.js
@@ -27,8 +27,90 @@ for (const [key,value] of map){
 }
 
 const myObject = {                    /// object is not iterable in 'forof' loop
-    game1 = "counterstrike"
-    game2 = "bgmi"
+    game1: "counterstrike",
+    game2: "bgmi"
 }
 
 /////////////// we will learn how to iterate objects on for loops 
+
+const myObject1 = {
+    js: "java script",
+    py: "python",
+    cpp: "C++"
+}
+
+for (const key in myObject1) {
+   // console.log(`${key} and full name ${myObject1[key]}`)
+}
+
+const programming = ["js", "python", "cpp", "java"]
+
+for (const key in programming) {
+    // console.log(`array printing in for in loop ${programming[key]}`)
+}
+
+///////////////////////   For each loop ///////////////////////
+
+const advanceprogram = ["js", "python", "cpp", "java", "AI", "GPT"]
+
+// advanceprogram.forEach( function (val) {
+//     console.log(val);
+// })
+
+// advanceprogram.forEach( (val) => {
+//      console.log(val);
+//  })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// advanceprogram.forEach(printMe)
+
+advanceprogram.forEach( (val, index, arr) => {
+ //    console.log(val, index, arr);
+ })
+
+
+const complexprogram = [
+    {
+        file: "python",
+        filename: "py"
+    },
+    {
+        file: "java",
+        filename: "java"
+    },
+    {
+        file: "c++",
+        filename: "cpp"
+    }
+]
+
+complexprogram.forEach( (val)=> {
+ //   console.log(val.filename);
+})
+
+const values = advanceprogram.forEach( (val) => {
+    return val
+})
+
+//console.log(values);
+
+
+const myNum = [1,2,3,4,7,8,9,10]
+
+// const numvalue = myNum.filter( (val) => {       //  ((val) => val > 4) 
+//     return val > 4
+// })
+// console.log(numvalue);
+
+
+const numvalue = []
+
+myNum.forEach( (val) => {
+    if(val > 4){
+        numvalue.push(val)
+    }
+})
+// console.log(numvalue);
